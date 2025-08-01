51d2c0368db0442b900686b39f156dbc848c6600
Sanjay <sanjaypersonal2007@gmail.com>
2025-08-02 01:06:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,16 @@ chat_id = os.environ['XLSXNSE_CHAT_ID']
 
 stocks = ['SOUTHBANK', 'IDFCFIRSTB', 'PGINVIT', 'GOLDBEES', 'MRF']
 
-def get_stock_prices():
-    message = "----------> 📈 NSE STOCKS INFO 📈<----------\n"
-    for stock in stocks:
-        try:
-            data = nse_eq(stock)
-            ltp = data['priceInfo']['lastPrice']
-            message += f"{stock}: ₹{ltp}\n"
-        except Exception as e:
-            message += f"{stock}: ❌ Error fetching data\n"
-
-    send_telegram(message)
-
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    payload = {'chat_id': chat_id, 'text': msg}
-    requests.post(url, data=payload)
+    payload = {
+        'chat_id': chat_id,
+        'text': msg,
+        'parse_mode': 'Markdown'
+    }
+    response = requests.post(url, data=payload)
+    print("Telegram response:", response.text)
 
-if __name__ == "__main__":
     get_stock_prices()
+
 
